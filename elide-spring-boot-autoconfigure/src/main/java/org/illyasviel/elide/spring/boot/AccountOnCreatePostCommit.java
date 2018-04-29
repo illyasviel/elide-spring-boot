d8efb1860725fabdd780178a80bfc1e60466ddd1
olOwOlo <26087907+olOwOlo@users.noreply.github.com>
2018-04-29 20:43:37 +0800

    how to register function hook ?

---
+++ elide-spring-boot-autoconfigure/src/main/java/org/illyasviel/elide/spring/boot/AccountOnCreatePostCommit.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.illyasviel.elide.spring.boot;
+
+import com.yahoo.elide.functions.LifeCycleHook;
+import com.yahoo.elide.security.ChangeSpec;
+import com.yahoo.elide.security.RequestScope;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author olOwOlo
+ */
+public class AccountOnCreatePostCommit implements LifeCycleHook<Account> {
+
+  private static final Logger logger = LoggerFactory.getLogger(AccountOnCreatePostCommit.class);
+
+  @Override
+  public void execute(Account account, RequestScope requestScope,
+      Optional<ChangeSpec> changes) {
+    logger.debug("AccountOnCreatePostCommit LifeCycleHook execute");
+  }
+}
--- elide-spring-boot-autoconfigure/src/main/java/org/illyasviel/elide/spring/boot/autoconfigure/ElideAutoConfiguration.java
+++ elide-spring-boot-autoconfigure/src/main/java/org/illyasviel/elide/spring/boot/autoconfigure/ElideAutoConfiguration.java
@@ -19,16 +19,26 @@ package org.illyasviel.elide.spring.boot.autoconfigure;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yahoo.elide.Elide;
 import com.yahoo.elide.ElideSettingsBuilder;
+import com.yahoo.elide.annotation.OnCreatePostCommit;
+import com.yahoo.elide.annotation.OnCreatePreCommit;
+import com.yahoo.elide.annotation.OnCreatePreSecurity;
 import com.yahoo.elide.core.DataStore;
 import com.yahoo.elide.core.EntityDictionary;
 import com.yahoo.elide.core.filter.dialect.RSQLFilterDialect;
+import com.yahoo.elide.functions.LifeCycleHook;
 import com.yahoo.elide.jsonapi.JsonApiMapper;
 import com.yahoo.elide.security.checks.Check;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.persistence.EntityManager;
 import org.atteo.classindex.ClassIndex;
 import org.hibernate.ScrollMode;
+import org.illyasviel.elide.spring.boot.Account;
+import org.illyasviel.elide.spring.boot.AccountOnCreatePostCommit;
+import org.illyasviel.elide.spring.boot.AccountOnCreatePreCommit;
 import org.illyasviel.elide.spring.boot.annotation.ElideCheck;
+import org.illyasviel.elide.spring.boot.annotation.ElideHook;
 import org.illyasviel.elide.spring.boot.datastore.SpringHibernateDataStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,6 +49,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -58,6 +69,7 @@ public class ElideAutoConfiguration {
   @ConditionalOnMissingBean
   public Elide elide(PlatformTransactionManager txManager,
       AutowireCapableBeanFactory beanFactory,
+      ApplicationContext context,
       EntityManager entityManager,
       ObjectMapper objectMapper,
       ElideProperties elideProperties) {
@@ -75,6 +87,57 @@ public class ElideAutoConfiguration {
 
     EntityDictionary entityDictionary = new EntityDictionary(checks);
     RSQLFilterDialect rsqlFilterDialect = new RSQLFilterDialect(entityDictionary);
+
+    // scan life cycle hooks
+    for (Class<?> clazz : ClassIndex.getAnnotated(ElideHook.class)) {
+      if (LifeCycleHook.class.isAssignableFrom(clazz)) {
+        ElideHook elideHook = clazz.getAnnotation(ElideHook.class);
+        // get generic entity type
+        Class<?> entity = null;
+        for (Type genericInterface : clazz.getGenericInterfaces()) {
+          if (genericInterface instanceof ParameterizedType
+              && ((ParameterizedType) genericInterface).getRawType().equals(LifeCycleHook.class)) {
+            Type[] genericTypes = ((ParameterizedType) genericInterface).getActualTypeArguments();
+            entity = (Class<?>) genericTypes[0];
+          }
+        }
+        if (entity == null) {
+          throw new RuntimeException("entity is null, this should not be thrown");
+        }
+
+        if (elideHook.fieldOrMethodName().equals("")) {
+          entityDictionary.bindTrigger(entity, elideHook.lifeCycle(),
+              (LifeCycleHook) context.getBean(clazz));
+        } else {
+          entityDictionary.bindTrigger(entity, elideHook.lifeCycle(),
+              elideHook.fieldOrMethodName(), (LifeCycleHook) context.getBean(clazz));
+        }
+
+        // temp: log
+        AccountOnCreatePreCommit bean = ((AccountOnCreatePreCommit) context.getBean(clazz));
+        bean.logString();
+
+        logger.debug("getBean: {}", bean);
+        logger.debug("getTriggers: {}", entityDictionary.getTriggers(Account.class, OnCreatePreCommit.class, ""));
+        // end temp
+
+        logger.debug("Register Elide Function Hook: bindTrigger({}, {}, \"{}\", {})",
+            entity.getCanonicalName(),
+            elideHook.lifeCycle().getSimpleName(),
+            //clazz.getCanonicalName(),
+            elideHook.fieldOrMethodName(),
+            bean.getClass().getCanonicalName());
+
+      } else {
+        throw new RuntimeException("ElideHook class must implements LifeCycleHook<T>");
+      }
+    }
+
+    // temp: bind some triggers
+    entityDictionary.bindTrigger(Account.class, OnCreatePostCommit.class, new AccountOnCreatePostCommit());
+    entityDictionary.bindTrigger(Account.class, OnCreatePreSecurity.class,
+        (a, b, c) -> logger.debug("OnCreatePreSecurity lambda execute"));
+    // end temp
 
     DataStore springDataStore = new SpringHibernateDataStore(txManager, beanFactory, entityManager,
         true, ScrollMode.FORWARD_ONLY);
